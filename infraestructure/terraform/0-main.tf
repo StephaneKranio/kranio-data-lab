c00d6179dd9cfab2a35e397ba84b22995e23f9c7
Stéphane Rongier <90786356+StephRoro@users.noreply.github.com>
2025-02-05 15:26:27 -0500

    git action deploy terraform added

--- infraestructure/terraform/0-main.tf
+++ infraestructure/terraform/0-main.tf
@@ -8,15 +8,15 @@
 #}
 
 
-#provider "google" {}
+provider "google" {}
 
 
-provider "google" {
-  project = "kranio-data-lab"
-  credentials = file("credentials/kranio-data-lab-b46e531119a7.json")
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
+#provider "google" {
+#  project = "kranio-data-lab"
+#  credentials = file("credentials/kranio-data-lab-b46e531119a7.json")
+#  region  = "us-central1"
+#  zone    = "us-central1-c"
+#}
 
 terraform {
   backend "gcs" {
