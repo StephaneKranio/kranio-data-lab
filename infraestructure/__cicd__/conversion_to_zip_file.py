a6769868faaa719fd79804c31c420610e8347267
Stéphane Rongier <90786356+StephRoro@users.noreply.github.com>
2025-02-10 15:23:43 -0500

    git action deploy terraform added test

--- infraestructure/__cicd__/conversion_to_zip_file.py
+++ infraestructure/__cicd__/conversion_to_zip_file.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 #env = sys.argv[1]
 
-origin_path_source_prd = '../../data-services'
+origin_path_source_prd = 'data-services'
 commons_path_source = "04-cloud_functions"
 origin_path_output = f'terraform/cloud-functions-files'
 path = Path(origin_path_output)
