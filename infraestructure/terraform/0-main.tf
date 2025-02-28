cbb1918c6aa312b1b8d7dff784a18fe99e41f1cc
Stéphane Rongier <90786356+StephRoro@users.noreply.github.com>
2025-02-28 10:36:38 -0500

    test

--- infraestructure/terraform/0-main.tf
+++ infraestructure/terraform/0-main.tf
@@ -1,3 +1,31 @@
+
+provider "google" {}
+
+
+terraform {
+  backend "gcs" {
+    bucket  = "kranio-data-lab-terraform-state"
+    prefix  = "terraform/state"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #terraform {
 #  required_providers {
 #    google = {
@@ -8,19 +36,9 @@
 #}
 
 
-provider "google" {}
-
-
 #provider "google" {
 #  project = "kranio-data-lab"
 #  credentials = file("credentials/kranio-data-lab-b46e531119a7.json")
 #  region  = "us-central1"
 #  zone    = "us-central1-c"
 #}
-
-terraform {
-  backend "gcs" {
-    bucket  = "kranio-data-lab-terraform-state"
-    prefix  = "terraform/state"
-  }
-}
